68b9db5a76c4aa5e42ea92750be700a7e988504e
Arun Sharma <digilinkers@gmail.com>
2025-12-02 09:32:46 +0530

    EGEONE Home Page Half Done ( 1 December)

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -9,8 +9,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
 
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light dd-nav">
     <div class="container">
-        <a class="navbar-brand" href="#">
-            <img src="" alt="">
+        <a class="navbar-brand" href="">
+            <img src="img/logo.png" alt="" class="img-fluid logo-img">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span> logo.png
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
@@ -14,6 +14,12 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact us</a>
                 </li>
                 @if (Auth::check())
                     <li class="nav-item dropdown">
